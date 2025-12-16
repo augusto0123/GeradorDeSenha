2596a20ea7e1cc6158461defb45f359ef8f28f39
Augusto <senescal.oliveira@gmail.com>
2025-12-15 22:14:31 -0300

    feat: adicao da geracao de senha

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
+// Seleção de elementos
+
 const range = document.querySelector("#tamanho");
 const value = document.querySelector("#valor-range");
+const senhaGerada = document.querySelector("#senha-gerada");
+const gerarBtn = document.querySelector("#gerar-btn");
 
 value.innerText = range.value;
+
+// Funções
+
+function gerarSenha(tamanho){
+    const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*+=/";
+    let senha = "";
+
+    for(let i = 0; i < tamanho; i++){
+        const index = Math.floor(Math.random() * caracteres.length);
+        senha+= caracteres[index];
+    }
+
+    return senha;
+}
+
+// Eventos
 
 range.addEventListener("input", () => {
   value.innerText = range.value;
 });
+
+gerarBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const tamanho = Number(range.value);
+    const senha = gerarSenha(tamanho);
+
+    senhaGerada.innerText = senha;
+    senhaGerada.classList.remove("hide");
+})
+
+
