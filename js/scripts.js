37f74cb6ac0fa67e5eeaf7a3148a87b4703ed1c2
Augusto <senescal.oliveira@gmail.com>
2025-12-15 22:53:31 -0300

    feat: adicao da funcionalidade de feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+const data = [
+    {
+        min:0,
+        max:8,
+        feedback: "senha fraca"
+    },
+    {
+        min:9,
+        max:15,
+        feedback: "senha forte"
+    },
+    {
+        min:16,
+        max:30,
+        feedback: "senha muito forte"
+    }
+];
+
 // Seleção de elementos
 
 const range = document.querySelector("#tamanho");
 const value = document.querySelector("#valor-range");
 const senhaGerada = document.querySelector("#senha-gerada");
 const gerarBtn = document.querySelector("#gerar-btn");
+const feedbackSenhaGerada = document.querySelector("#feedback-senha");
 
 value.innerText = range.value;
 
@@ -17,8 +36,19 @@ function gerarSenha(tamanho){
         const index = Math.floor(Math.random() * caracteres.length);
         senha+= caracteres[index];
     }
-
     return senha;
+}
+
+function feedbackSenha(tamanho){
+    let feedback;
+
+    data.forEach((item) => {
+        if (tamanho >= item.min && tamanho <= item.max){
+            feedback = item.feedback;
+        }
+    })
+
+    return feedback;
 }
 
 // Eventos
@@ -32,9 +62,26 @@ gerarBtn.addEventListener("click", (e) => {
 
     const tamanho = Number(range.value);
     const senha = gerarSenha(tamanho);
+    const feedback = feedbackSenha(tamanho);
 
     senhaGerada.innerText = senha;
     senhaGerada.classList.remove("hide");
-})
+    feedbackSenhaGerada.innerText = feedback;
+
+    feedbackSenhaGerada.classList.remove(
+        "senha-fraca",
+        "senha-forte",
+        "senha-muito-forte"
+        );
+
+        if (tamanho <= 8) {
+            feedbackSenhaGerada.classList.add("senha-fraca");
+        } else if (tamanho <= 15) {
+            feedbackSenhaGerada.classList.add("senha-forte");
+        } else {
+            feedbackSenhaGerada.classList.add("senha-muito-forte");
+        }
+
+});
 
 
